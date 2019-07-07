57cf5af892180854ebc38e67e9d5c1398521be22
pukovnikkostadinovic <pukovnikkostadinovic@gmail.com>
2019-07-07 20:10:44 +0200

    svasta

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize('baza_komponenti2', 'root', 'dlibreman',{host:'localhost', dialect:'mysql', operatorsAliases: false});
+const sequelize = new Sequelize('baza_komponenti2', 'dlibreman', 'dlibreman',{host:'localhost', dialect:'mysql', operatorsAliases: false});
 
 module.exports = sequelize;
 
